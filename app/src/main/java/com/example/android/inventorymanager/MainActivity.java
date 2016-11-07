6d785281f92af1dd79d8ec0c4430805e23ef8cc3
rdl1990@live.co.uk <rdl1990@live.co.uk>
2016-11-07 22:08:25 +0000

    Added empty view message for main activity

--- app/src/main/java/com/example/android/inventorymanager/MainActivity.java
+++ app/src/main/java/com/example/android/inventorymanager/MainActivity.java
@@ -2,12 +2,21 @@ package com.example.android.inventorymanager;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_inventory);
+
+        // Find the ListView which will be populated with the pet data
+        ListView inventoryListView = (ListView) findViewById(R.id.list);
+
+        // Find and set empty view on the ListView, so that it only shows when the list has 0 items.
+        View emptyView = findViewById(R.id.empty_view);
+        inventoryListView.setEmptyView(emptyView);
     }
 }
